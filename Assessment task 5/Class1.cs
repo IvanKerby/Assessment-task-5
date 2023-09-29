e251e680ca4a05fc525f71e1652b75fbacc97820
pedri <pedri@LELOUCH>
2023-09-29 21:40:46 +1300

    Add project files.

---
+++ Assessment task 5/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_task_5
+{
+    class Student
+    {
+        // Properties to store subject marks
+        public double MathMarks { get; set; }
+        public double PhysicsMarks { get; set; }
+        public double ChemistryMarks { get; set; }
+        public double ComputerScienceMarks { get; set; }
+
+        // Method to calculate the average marks
+        public double calculateaverage()
+        {
+            // Calculate the average by summing all subject marks and dividing by the number of subjects
+            return (MathMarks + PhysicsMarks + ChemistryMarks + ComputerScienceMarks) / 4;
+        }
+
+        // Method to calculate the grade based on the average marks
+        public char calculategrade()
+        {
+            // Calculate the average marks
+            double average = calculateaverage();
+
+            // Determine the grade based on the average marks
+            if (average >= 80)
+                return 'A';
+            else if (average >= 70)
+                return 'B';
+            else if (average >= 60)
+                return 'C';
+            else if (average >= 50)
+                return 'D';
+            else
+                return 'F';
+        }
+
+        // Method to get remarks based on the grade
+        public string getremarks()
+        {
+            // Calculate the grade
+            char grade = calculategrade();
+
+            // Provide remarks based on the grade using a switch statement
+            switch (grade)
+            {
+                case 'A':
+                    return "Your grade is A";
+                case 'B':
+                    return "Your grade is B";
+                case 'C':
+                    return "Your grade is C";
+                case 'D':
+                    return "Your grade is D";
+                case 'F':
+                    return "Your grade is F";
+                default:
+                    return "Invalid grade";
+            }
+        }
+    }
+}
+
---
+++ Assessment task 5/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Prompt the user to enter marks for various subjects
+                Console.WriteLine("Enter marks for the following subjects (out of 100):");
+
+                // Read and store the marks for Math
+                Console.Write("Maths: ");
+                double mathmarks = double.Parse(Console.ReadLine());
+
+                // Read and store the marks for Physics
+                Console.Write("Physics: ");
+                double physicsmarks = double.Parse(Console.ReadLine());
+
+                // Read and store the marks for Chemistry
+                Console.Write("Chemistry: ");
+                double chemistrymarks = double.Parse(Console.ReadLine());
+
+                // Read and store the marks for Computer Science
+                Console.Write("Computer Science: ");
+                double computersciencemarks = double.Parse(Console.ReadLine());
+
+                // Create a new 'Student' object with subject marks
+                var student = new Student
+                {
+                    MathMarks = mathmarks,
+                    PhysicsMarks = physicsmarks,
+                    ChemistryMarks = chemistrymarks,
+                    ComputerScienceMarks = computersciencemarks
+                };
+
+                // Calculate the average marks for the student
+                double average = student.calculateaverage();
+
+                // Calculate the grade based on the average marks
+                char grade = student.calculategrade();
+
+                // Get remarks based on the grade
+                string remarks = student.getremarks();
+
+                // Display the average marks, grade, and remarks
+                Console.WriteLine("Average Marks: " + average);
+                Console.WriteLine("Grade: " + grade);
+                Console.WriteLine(remarks);
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions, if any, and display an error message
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            // Wait for user input (e.g., pressing a key) before exiting
+            Console.ReadKey();
+        }
+    }
+}
